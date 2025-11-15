d79af2d556e9dc236631d11a23cc5f378654d3da
Benjamin Lehr <b.lehr@trio-group.de>
2025-11-15 22:27:09 +0100

    Add Avatar and Dropdown components; refactor Menu handling
    Introduced new `Avatar` and `Dropdown` components to improve UI modularity and functionality. Refactored `Menu` component to align with updated `menuVariants`, renamed constants, and enhanced flexibility with additional properties and dynamic class handling.

--- src/index.ts
+++ src/index.ts
@@ -11,3 +11,12 @@ export {default as CardActions} from './components/card/CardActions.vue';
 export {default as CardFigure} from './components/card/CardFigure.vue';
 export {default as CardBody} from './components/card/CardBody.vue';
 export {default as CardTitle} from './components/card/CardTitle.vue';
+
+//Avatar
+export {default as Avatar} from './components/avatar/Avatar.vue';
+export {default as AvatarGroup} from './components/avatar/AvatarGroup.vue';
+
+
+//Avatar
+export {default as Dropdown} from './components/dropdown/Dropdown.vue';
+
