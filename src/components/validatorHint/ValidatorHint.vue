bcfcb0643891e0bebb2489a0340de8aafba3b5ce
Pascal Akhidue <pascal_akhidue@hotmail.com>
2025-11-20 18:23:16 +0100

    TRIA2250230-64 | adds cn helper

--- src/components/validatorHint/ValidatorHint.vue
+++ src/components/validatorHint/ValidatorHint.vue
@@ -1,11 +1,20 @@
 <script setup lang="ts">
+import {computed, HTMLAttributes} from "vue";
+import {cn} from "@/lib/utils";
+
+const props = defineProps<{
+    class?: HTMLAttributes['class']
+}>();
+
+const classes = computed(() => cn('validator-hint', props.class));
+
 defineProps<{
     message?: string
 }>();
 </script>
 
 <template>
-    <p class="validator-hint">
+    <p :class="classes">
         <slot>{{ message }}</slot>
     </p>
 </template>
